9d6e8848ee315f972425250daf279615095edf65
sergeyshappo <shappoff@gmail.com>
2024-02-18 19:22:21 +0000

    Update from https://github.com/sergeyshappo/blog/commit/32dd47a7923e604bc0e68a768260a3e1dcca1fc9

--- components/MapComponent.d.ts
+++ components/MapComponent.d.ts
@@ -1,4 +1,4 @@
 import React from "react";
 import 'leaflet-sidebar';
 import 'leaflet-sidebar/src/L.Control.Sidebar.css';
-export declare const MapComponent: ({ hits, facets }: any) => React.JSX.Element;
+export declare const MapComponent: ({ dataMinsk, dataZalazje, facets }: any) => React.JSX.Element;
