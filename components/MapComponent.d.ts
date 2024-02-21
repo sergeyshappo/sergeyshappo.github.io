b0f259e2fa5013ea18e73123e474570a38aa451d
sergeyshappo <shappoff@gmail.com>
2024-02-21 12:20:02 +0000

    Update from https://github.com/sergeyshappo/blog/commit/f89bf6a497b58335f4bfcaa6da1747ce058c532a

--- components/MapComponent.d.ts
+++ components/MapComponent.d.ts
@@ -1,4 +1,6 @@
 import React from "react";
+import "leaflet/dist/leaflet.css";
+import "leaflet-draw/dist/leaflet.draw.css";
 import 'leaflet-sidebar';
 import 'leaflet-sidebar/src/L.Control.Sidebar.css';
 export declare const MapComponent: ({ data }: any) => React.JSX.Element;
