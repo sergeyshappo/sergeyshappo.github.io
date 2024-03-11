d7cbe266d113970b19f9ff5e149b7c1d8d7751dd
sergeyshappo <shappoff@gmail.com>
2024-03-11 11:38:56 +0000

    Update from https://github.com/sergeyshappo/blog/commit/d102737747bf010fc139d242975dfac8f6f77ec9

---
+++ components/icons.d.ts
@@ -0,0 +1,5 @@
+export declare const pinIcon: () => string;
+export declare const circleIcon: ({ color }: any) => string;
+export declare const houseIcon: ({ color }: any) => string;
+export declare const schoolIcon: ({ color }: any) => string;
+export declare const getIcon: (type: string) => ({ color }: any) => string;
